57fcb15780d7c74811106cdde7986c16d9e8f592
nhman2002 <nhman2002@gmail.com>
2023-03-31 23:34:00 +0700

    tạo lại DB_ENG_VER

--- SCRIPT/Database_eng_ver.sql
+++ SCRIPT/Database_eng_ver.sql
@@ -1,6 +1,6 @@
-﻿create database HQTCSDL2
+﻿create database HQTCSDL1
 go
-use HQTCSDL2
+use HQTCSDL1
 go
 
 BEGIN TRY
@@ -10,11 +10,11 @@ BEGIN TRAN;
 -- CreateTable
 CREATE TABLE [dbo].[Account] (
     [id] INT NOT NULL IDENTITY(1,1),
+    [username] NVARCHAR(1000) NOT NULL UNIQUE,
     [password] NVARCHAR(1000) NOT NULL,
-    [username] NVARCHAR(1000) NOT NULL,
-    [role] NVARCHAR(1000) NOT NULL,
+    [role] NVARCHAR(1000) NOT NULL ,
     [confirmed] BIT NOT NULL CONSTRAINT [Account_confirmed_df] DEFAULT 0,
-    [confirmCode] NVARCHAR(1000),
+    [confirmCode] NVARCHAR(1000) UNIQUE,
     [status] NVARCHAR(1000) NOT NULL CONSTRAINT [Account_status_df] DEFAULT 'active',
     CONSTRAINT [Account_pkey] PRIMARY KEY CLUSTERED ([id]),
     CONSTRAINT [Account_username_key] UNIQUE NONCLUSTERED ([username])
@@ -24,10 +24,10 @@ CREATE TABLE [dbo].[Account] (
 CREATE TABLE [dbo].[Customer] (
     [id] INT NOT NULL IDENTITY(1,1),
     [accountId] INT NOT NULL,
-    [email] NVARCHAR(1000) NOT NULL,
     [name] NVARCHAR(1000) NOT NULL,
-    [phone] NVARCHAR(1000) NOT NULL,
     [address] NVARCHAR(1000) NOT NULL,
+    [phone] NVARCHAR(1000) NOT NULL,
+    [email] NVARCHAR(1000) NOT NULL,
     CONSTRAINT [Customer_pkey] PRIMARY KEY CLUSTERED ([id]),
     CONSTRAINT [Customer_accountId_key] UNIQUE NONCLUSTERED ([accountId]),
     CONSTRAINT [Customer_email_key] UNIQUE NONCLUSTERED ([email]),
@@ -39,12 +39,12 @@ CREATE TABLE [dbo].[Partner] (
     [id] INT NOT NULL IDENTITY(1,1),
     [accountId] INT NOT NULL,
     [contractId] INT,
-    [brandName] NVARCHAR(1000) NOT NULL,
     [email] NVARCHAR(1000) NOT NULL,
-    [phone] NVARCHAR(1000) NOT NULL,
     [bankAccount] NVARCHAR(1000) NOT NULL,
     [representative] NVARCHAR(1000),
+    [phone] NVARCHAR(1000) NOT NULL,
     [orderQuantity] INT,
+    [brandName] NVARCHAR(1000) NOT NULL,
     [status] NVARCHAR(1000),
     [culinaryStyle] NVARCHAR(1000),
     CONSTRAINT [Partner_pkey] PRIMARY KEY CLUSTERED ([id]),
@@ -63,8 +63,8 @@ CREATE TABLE [dbo].[Shipper] (
     [name] NVARCHAR(1000) NOT NULL,
     [nationalId] NVARCHAR(1000) NOT NULL,
     [phone] NVARCHAR(1000) NOT NULL,
-    [address] NVARCHAR(1000),
     [licensePlate] NVARCHAR(1000),
+    [address] NVARCHAR(1000),
     [bankAccount] NVARCHAR(1000),
     CONSTRAINT [Shipper_pkey] PRIMARY KEY CLUSTERED ([id]),
     CONSTRAINT [Shipper_accountId_key] UNIQUE NONCLUSTERED ([accountId]),
@@ -128,10 +128,10 @@ CREATE TABLE [dbo].[Order] (
     [customerId] INT NOT NULL,
     [shipperId] INT,
     [partnerId] INT NOT NULL,
-    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Order_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
-    [deliveredAt] DATETIME2,
     [status] NVARCHAR(1000) NOT NULL CONSTRAINT [Order_status_df] DEFAULT 'pending',
     [process] NVARCHAR(1000) NOT NULL CONSTRAINT [Order_process_df] DEFAULT 'pending',
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Order_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [deliveredAt] DATETIME2,
     [orderPrice] FLOAT(53) NOT NULL,
     [shippingPrice] FLOAT(53),
     CONSTRAINT [Order_pkey] PRIMARY KEY CLUSTERED ([id])
@@ -166,11 +166,11 @@ CREATE TABLE [dbo].[District] (
 
 -- CreateTable
 CREATE TABLE [dbo].[Rating] (
-    [isLike] BIT NOT NULL,
+    [customerId] INT NOT NULL,
     [description] NVARCHAR(1000),
     [createdAt] DATETIME2 NOT NULL CONSTRAINT [Rating_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
     [updatedAt] DATETIME2 NOT NULL,
-    [customerId] INT NOT NULL,
+    [isLike] BIT NOT NULL,
     [dishId] INT NOT NULL,
     CONSTRAINT [Rating_pkey] PRIMARY KEY CLUSTERED ([customerId],[dishId])
 );
@@ -223,6 +223,9 @@ ALTER TABLE [dbo].[Rating] ADD CONSTRAINT [Rating_customerId_fkey] FOREIGN KEY (
 -- AddForeignKey
 ALTER TABLE [dbo].[Rating] ADD CONSTRAINT [Rating_dishId_fkey] FOREIGN KEY ([dishId]) REFERENCES [dbo].[Dish]([id]) ON DELETE CASCADE ON UPDATE CASCADE;
 
+
+
+
 COMMIT TRAN;
 
 END TRY
@@ -237,103 +240,103 @@ THROW
 END CATCH
 
 
-INSERT INTO THANHPHO(TEN) OUTPUT inserted.ID values(N'Hồ Chí Minh')
-INSERT INTO THANHPHO(TEN) OUTPUT inserted.ID values(N'Hà Nội')
-INSERT INTO THANHPHO(TEN) OUTPUT inserted.ID values(N'Nha Trang')
-INSERT INTO THANHPHO(TEN) OUTPUT inserted.ID values(N'Đà Nẵng')
+INSERT INTO [dbo].[City]([name]) OUTPUT inserted.ID values(N'Hồ Chí Minh')
+INSERT INTO [dbo].[City]([name]) OUTPUT inserted.ID values(N'Hà Nội')
+INSERT INTO [dbo].[City]([name]) OUTPUT inserted.ID values(N'Nha Trang')
+INSERT INTO [dbo].[City]([name]) OUTPUT inserted.ID values(N'Đà Nẵng')
 
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(01,N'Quận 1')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(01,N'Quận 2')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(01,N'Quận 3')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(01,N'Quận 4')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(02,N'Quận Hoàn Kiếm')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(02,N'Quận Đống Đa')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(02,N'Quận Thanh Xuân')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(02,N'Quận Hà Đông')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(03,N'Vĩnh Hòa')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(03,N'Vĩnh Phước')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(03,N'Vĩnh Hải')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(03,N'Vĩnh Thọ')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(04,N'Hải Châu')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(04,N'Cẩm Lệ')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(04,N'Thanh Khê')
-INSERT INTO QUAN(ID_THANH_PHO,TEN) OUTPUT inserted.ID values(04,N'Sơn Trà')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(01,N'Quận 1')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(01,N'Quận 2')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(01,N'Quận 3')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(01,N'Quận 4')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(02,N'Quận Hoàn Kiếm')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(02,N'Quận Đống Đa')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(02,N'Quận Thanh Xuân')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(02,N'Quận Hà Đông')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(03,N'Vĩnh Hòa')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(03,N'Vĩnh Phước')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(03,N'Vĩnh Hải')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(03,N'Vĩnh Thọ')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(04,N'Hải Châu')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(04,N'Cẩm Lệ')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(04,N'Thanh Khê')
+INSERT INTO [dbo].[District]([cityId],[name]) OUTPUT inserted.ID values(04,N'Sơn Trà')
 
-INSERT INTO TAIKHOAN OUTPUT inserted.ID values ('taixe1','taixe','taixe','active')
-INSERT INTO TAIKHOAN OUTPUT inserted.ID values ('taixe2','taixe','taixe','active')
-INSERT INTO TAIKHOAN OUTPUT inserted.ID values ('taixe3','taixe','taixe','active')
-INSERT INTO TAIKHOAN OUTPUT inserted.ID values ('khach1','khachhang','khachhang','active')
-INSERT INTO TAIKHOAN OUTPUT inserted.ID values ('khach2','khachhang','khachhang','active')
-INSERT INTO TAIKHOAN OUTPUT inserted.ID values ('khach3','khachhang','khachhang','active')
-INSERT INTO TAIKHOAN OUTPUT inserted.ID values ('doitac1','doitac','doitac','active')
-INSERT INTO TAIKHOAN OUTPUT inserted.ID values ('doitac2','doitac','doitac','active')
-INSERT INTO TAIKHOAN OUTPUT inserted.ID values ('doitac3','doitac','doitac','active')
+INSERT INTO [dbo].[Account](username, [password], [role],[confirmed],[confirmCode], [status]) OUTPUT inserted.ID values ('taixe1','taixe','taixe',0,'123456','active')
+INSERT INTO [dbo].[Account](username, [password], [role],[confirmed],[confirmCode], [status]) OUTPUT inserted.ID values ('taixe2','taixe','taixe',0,'312645','active')
+INSERT INTO [dbo].[Account](username, [password], [role],[confirmed],[confirmCode], [status]) OUTPUT inserted.ID values ('taixe3','taixe','taixe',0,'213456','active')
+INSERT INTO [dbo].[Account](username, [password], [role],[confirmed],[confirmCode], [status]) OUTPUT inserted.ID values ('khach1','khachhang','khachhang',0,'987654','active')
+INSERT INTO [dbo].[Account](username, [password], [role],[confirmed],[confirmCode], [status]) OUTPUT inserted.ID values ('khach2','khachhang','khachhang',0,'978564','active')
+INSERT INTO [dbo].[Account](username, [password], [role],[confirmed],[confirmCode], [status]) OUTPUT inserted.ID values ('khach3','khachhang','khachhang',0,'983671','active')
+INSERT INTO [dbo].[Account](username, [password], [role],[confirmed],[confirmCode], [status]) OUTPUT inserted.ID values ('doitac1','doitac','doitac',0,'382914','active')
+INSERT INTO [dbo].[Account](username, [password], [role],[confirmed],[confirmCode], [status]) OUTPUT inserted.ID values ('doitac2','doitac','doitac',0,'917823','active')
+INSERT INTO [dbo].[Account](username, [password], [role],[confirmed],[confirmCode], [status]) OUTPUT inserted.ID values ('doitac3','doitac','doitac',0,'123753','active')
 
-INSERT INTO TAIXE OUTPUT inserted.ID values (01,02,N'Nguyễn Văn A','123123321321','0123456789', '59D657892','nva@gmail.com', '9867986712341234')
-INSERT INTO TAIXE OUTPUT inserted.ID values (02,02,N'Nguyễn Văn B','758111928099','0987654321', '59F123456','nvb@gmail.com', '4536728192873024')
-INSERT INTO TAIXE OUTPUT inserted.ID values (03,04,N'Nguyễn Văn C','102111928293','0123123421', '59A987654','nvc@gmail.com', '1234897859172101')
+INSERT INTO [dbo].[Shipper] OUTPUT inserted.ID values (01,02,N'Nguyễn Văn A','123123321321','0123456789', '59D657892','227 Nguyen Van Cu', '9867986712341234')
+INSERT INTO [dbo].[Shipper] OUTPUT inserted.ID values (02,02,N'Nguyễn Văn B','758111928099','0987654321', '59F123456','227 Dien Bien Phu', '4536728192873024')
+INSERT INTO [dbo].[Shipper] OUTPUT inserted.ID values (03,04,N'Nguyễn Văn C','102111928293','0123123421', '59A987654','123 Tran Hung Dao', '1234897859172101')
 
-INSERT INTO HOPDONG OUTPUT inserted.ID values('8271892819', N'Nguyễn Huỳnh Mẫn', '551717', '9817293085828371', GETDATE(), 0, 0, null, null)
-INSERT INTO HOPDONG OUTPUT inserted.ID values('8291829187', N'Nguyễn Hồ Trung Hiếu', '1231223', '4627110829189840', GETDATE(), 0, 0, null, null)
-INSERT INTO HOPDONG OUTPUT inserted.ID values('1829908118', N'Thiều Vĩnh Trung', '551718', '40198568920129130', GETDATE(), 0, 0, null, null)
+INSERT INTO [dbo].[Contract] OUTPUT inserted.ID values( GETDATE(),null,null,0,0,'8271892819', N'Nguyễn Huỳnh Mẫn', '551717', '9817293085828371')
+INSERT INTO [dbo].[Contract] OUTPUT inserted.ID values( GETDATE(),null,null,0,0,'8291829187', N'Nguyễn Hồ Trung Hiếu', '1231223', '4627110829189840')
+INSERT INTO [dbo].[Contract] OUTPUT inserted.ID values( GETDATE(),null,null,0,0,'1829908118', N'Thiều Vĩnh Trung', '551718', '40198568920129130')
 
-INSERT INTO DOITAC OUTPUT inserted.ID values (07, 01,'abc@gmail.com', '9817293085828371', N'Nguyễn Huỳnh Mẫn', '087691920192', 20, N'CAFE N GO','active','Nuoc')
-INSERT INTO DOITAC OUTPUT inserted.ID values (08, 02,'xyz@gmail.com', '4627110829189840', N'Nguyễn Hồ Trung Hiếu', '0777058016', 20, N'GO CAFE','active', 'Nuoc')
-INSERT INTO DOITAC OUTPUT inserted.ID values (09, 03,'def@gmail.com', '40198568920129130', N'Thiều Vĩnh Trung', '013458910291', 25, N'3 MIEN','active', 'Thuc An')
+INSERT INTO [dbo].[Partner] OUTPUT inserted.ID values (07, 01,'abc@gmail.com', '9817293085828371', N'Nguyễn Huỳnh Mẫn', '087691920192', 20, N'CAFE N GO','active','Nuoc')
+INSERT INTO [dbo].[Partner] OUTPUT inserted.ID values (08, 02,'xyz@gmail.com', '4627110829189840', N'Nguyễn Hồ Trung Hiếu', '0777058016', 20, N'GO CAFE','active', 'Nuoc')
+INSERT INTO [dbo].[Partner] OUTPUT inserted.ID values (09, 03,'def@gmail.com', '40198568920129130', N'Thiều Vĩnh Trung', '013458910291', 25, N'3 MIEN','active', 'Thuc An')
 
-INSERT INTO KHACHHANG OUTPUT inserted.ID values (04,N'Tran Thi A', N'400 Điện Biên Phủ, HCM','09810238912','hello@gmail.com')
-INSERT INTO KHACHHANG OUTPUT inserted.ID values (05,N'Tran Thi B', N'250 Trần Hưng đạo','09898798798','hello1@gmail.com')
-INSERT INTO KHACHHANG OUTPUT inserted.ID values (06,N'Tran Thi C', N'1000 Trường Chinh','09879872231','hello2@gmail.com')
+INSERT INTO [dbo].[Customer] OUTPUT inserted.ID values (04,N'Tran Thi A', N'400 Điện Biên Phủ, HCM','09810238912','hello@gmail.com')
+INSERT INTO [dbo].[Customer] OUTPUT inserted.ID values (05,N'Tran Thi B', N'250 Trần Hưng đạo','09898798798','hello1@gmail.com')
+INSERT INTO [dbo].[Customer] OUTPUT inserted.ID values (06,N'Tran Thi C', N'1000 Trường Chinh','09879872231','hello2@gmail.com')
 
-INSERT INTO CHINHANH OUTPUT inserted.ID values (01, 04, 1,'227 Nguyen Van Cu')
-INSERT INTO CHINHANH OUTPUT inserted.ID values (01, 01, 2,'210 Tran Hung Dao')
-INSERT INTO CHINHANH OUTPUT inserted.ID values (02, 02, 1,'550 Truong Son')
-INSERT INTO CHINHANH OUTPUT inserted.ID values (03, 01, 1,'100 Nam Ky Khoi Nghia')
+INSERT INTO [dbo].[Branch] OUTPUT inserted.ID values (01, 04, 1,'227 Nguyen Van Cu')
+INSERT INTO [dbo].[Branch] OUTPUT inserted.ID values (01, 01, 2,'210 Tran Hung Dao')
+INSERT INTO [dbo].[Branch] OUTPUT inserted.ID values (02, 02, 1,'550 Truong Son')
+INSERT INTO [dbo].[Branch] OUTPUT inserted.ID values (03, 01, 1,'100 Nam Ky Khoi Nghia')
 
-INSERT INTO MON OUTPUT inserted.ID values (01,N'Mì Soba',N'Mì lạnh', 'Con hang',null)
-INSERT INTO MON OUTPUT inserted.ID values (01,N'Mì gói',N'Mì hảo hảo','Con hang',null)
-INSERT INTO MON OUTPUT inserted.ID values (02,N'Mì ramen',N'Mì nước','Con hang',null)
-INSERT INTO MON OUTPUT inserted.ID values (02,N'Mì udon',N'Mì nước','Con hang',null)
-INSERT INTO MON OUTPUT inserted.ID values (03,N'Yakisoba',N'Mì xào','Con hang',null)
-INSERT INTO MON OUTPUT inserted.ID values (03,N'Katsudon',N'Thịt chiên','Con hang',null)
+INSERT INTO [dbo].[Dish] OUTPUT inserted.ID values (01,N'Soba Noodle',N'Cold Noodle', 'In stock',null)
+INSERT INTO [dbo].[Dish] OUTPUT inserted.ID values (01,N'Instant Noodle',N'Hảo Hảo Noodle','In stock',null)
+INSERT INTO [dbo].[Dish] OUTPUT inserted.ID values (02,N'Ramen Noodle',N'Water Noodle','In stock',null)
+INSERT INTO [dbo].[Dish] OUTPUT inserted.ID values (02,N'Udon Noodle',N'Water Noodle','In stock',null)
+INSERT INTO [dbo].[Dish] OUTPUT inserted.ID values (03,N'Yakisoba',N'Fried Noodle','In stock',null)
+INSERT INTO [dbo].[Dish] OUTPUT inserted.ID values (03,N'Katsudon',N'Fried Meat','In stock',null)
 
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (01,N'Mặc định', 20, 50000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (01,N'Thêm trứng', 20, 15000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (01,N'Thêm rau',20, 7000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (02,N'Mặc định', 20,50000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (02,N'Thêm trứng',20,15000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (02,N'Thêm rau',20,7000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (03,N'Mặc định', 20,50000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (03,N'Thêm trứng',20,15000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (03,N'Thêm rau',20,7000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (04,N'Mặc định', 20,50000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (04,N'Thêm trứng',20,15000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (04,N'Thêm rau',20,7000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (05,N'Mặc định', 20,50000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (05,N'Thêm trứng',20,15000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (05,N'Thêm rau',20,7000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (06,N'Mặc định', 20,50000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (06,N'Thêm trứng',20,15000)
-INSERT INTO TUYCHONMON OUTPUT inserted.ID values (06,N'Thêm rau',20,7000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (01,N'Default', 50000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (01,N'Add Egg', 15000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (01,N'Add green', 7000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (02,N'Default',50000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (02,N'Add Egg',15000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (02,N'Add green',7000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (03,N'Default',50000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (03,N'Add Egg',15000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (03,N'Add green',7000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (04,N'Default',50000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (04,N'Add Egg',15000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (04,N'Add green',7000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (05,N'Default',50000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (05,N'Add Egg',15000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (05,N'Add green',7000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (06,N'Default',50000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (06,N'Add Egg',15000)
+INSERT INTO [dbo].[DishDetail] OUTPUT inserted.ID values (06,N'Add green',7000)
 
-INSERT INTO DONHANG OUTPUT inserted.MADON values (01,01,01,'Xac nhan', 'Dang chuan bi', GETDATE(),null, 50000, 20000)
-INSERT INTO DONHANG OUTPUT inserted.MADON values (02,02,02,'Xac nhan', 'Dang chuan bi', GETDATE(),null, 50000, 25000)	
-INSERT INTO DONHANG OUTPUT inserted.MADON values (03,03,03,'Xac nhan', 'Dang chuan bi', GETDATE(),null, 50000, 25000)	
+INSERT INTO [dbo].[Order] OUTPUT inserted.ID values (01,null,01,'Pending', 'Pending', GETDATE(),null, 50000, 20000)
+INSERT INTO [dbo].[Order] OUTPUT inserted.ID values (02,null,02,'Pending', 'Pending', GETDATE(),null, 50000, 25000)	
+INSERT INTO [dbo].[Order] OUTPUT inserted.ID values (03,null,03,'Pending', 'Pending', GETDATE(),null, 50000, 25000)	
 
-INSERT INTO DANHGIA values (01,01,'Ngon',1)
-INSERT INTO DANHGIA values (02,01,'Ngon',1)
-INSERT INTO DANHGIA values (03,01,'Ngon',1)
+INSERT INTO [dbo].[Rating] values (01,'Delicious',GETDATE(),GETDATE(),1,1)
+INSERT INTO [dbo].[Rating] values (02,'Delicious',GETDATE(),GETDATE(),1,1)
+INSERT INTO [dbo].[Rating] values (03,'Delicious',GETDATE(),GETDATE(),1,1)
 --Tạo thêm danhgia
 
 --sửa lại unique ở id món,tùy chọn
-INSERT INTO CHITIETDONHANG values (1,01,2,100000)
-INSERT INTO CHITIETDONHANG values (1,02,3,100000)
-INSERT INTO CHITIETDONHANG values (1,04,2,100000)
-INSERT INTO CHITIETDONHANG values (2,02,2,100000)
-INSERT INTO CHITIETDONHANG values (2,05,2,100000)
+INSERT INTO [dbo].[OrderDetail] values (1,N'Soba Noodle','Default',01,100000)
+INSERT INTO [dbo].[OrderDetail] values (1,N'Instant Noodle','Default',02,100000)
+INSERT INTO [dbo].[OrderDetail] values (1,N'Ramen Noodle','Default',04,100000)
+INSERT INTO [dbo].[OrderDetail] values (2,N'Yakisoba','Default',02,100000)
+INSERT INTO [dbo].[OrderDetail] values (2,N'Katsudon','Default',05,100000)
 
 -- Used for drop the database
 --use master 
 --go
---alter database HQTCSDL2 set single_user with rollback immediate
---drop database HQTCSDL2
+--alter database HQTCSDL3 set single_user with rollback immediate
+--drop database HQTCSDL3
